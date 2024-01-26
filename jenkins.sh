ca30087211bf556e746968fb3c01fbe659540440
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2024-01-26 03:54:42 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,12 +6,12 @@ sudo apt upgrade -y
 
 sudo apt install openjdk-17-jre -y
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
+sudo apt-get update
 sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
