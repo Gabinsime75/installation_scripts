11844f03a55317f9a4843d043e0d439268104186
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2024-04-27 04:00:35 -0500

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-
 echo "Install Helm Chart Using Script"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-
 echo "checking version.."
 helm version
+
+# add official stable Helm charts repository for your local client.
+helm repo add stable https://charts.helm.sh/stable
+# Add prometheus Helm repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm search repo prometheus-community
+# Create Prometheus namespace
+kubectl create namespace prometheus
+
+# Install kube-prometheus-stack
+# Below is helm command to install kube-prometheus-stack. The helm repo kube-stack-prometheus (formerly prometheus-operator) comes with a grafana deployment embedded.
+helm install stable prometheus-community/kube-prometheus-stack -n prometheus
+# Lets check if prometheus and grafana pods are running already
+kubectl get pods -n prometheus
+kubectl get svc -n prometheus
+
+# In order to make prometheus and grafana available outside the cluster, use LoadBalancer or NodePort instead of ClusterIP.
+kubectl edit svc stable-kube-prometheus-sta-prometheus -n prometheus
+kubectl edit svc stable-grafana -n prometheus
+
+kubectl get svc -n prometheus
+
